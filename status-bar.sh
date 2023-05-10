746ffe2560017e7aaf9fd1d66d2b2b0b71a3bf72
eagledb14 <eagledb14@gmail.com>
2023-05-10 14:41:34 -0600

    fixed date spacing

--- status-bar.sh
+++ status-bar.sh
@@ -3,7 +3,7 @@
 
 while :
 do
-  date=$(date +"%a %b%e, %H:%M:%S")
+  date=$(date +"%a %b %e, %H:%M:%S")
   volume=$(pactl list sinks | grep -A 11 -e 'State: RUNNING' -e 'State: IDLE' | grep -m 1 Volume | awk '{print $5}' ORS='')
 
   echo "$date | Volume: $volume | "
