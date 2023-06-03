bfb021f9b88078c80025ed7780a618e0699c3d1d
eagledb14 <eagledb14@gmail.com>
2023-06-03 05:11:08 -0600

    can't assume .config exists, added it's creation

--- startup-command.sh
+++ startup-command.sh
@@ -12,6 +12,7 @@ cd
 # create directories
 mkdir ~/documents
 mkdir ~/Downloads
+mkdir ~/.config
 
 
 # upating before install
@@ -36,15 +37,8 @@ sudo rm -rf ~/go
 
 # install dotfiles
 cd ~/.config
-pass_file="./pass.sh"
 
-# echo "!#/bin/bash" > "$pass_file"
-# echo "exec echo $git_password" >> "$pass_file"
-# chmod +x "$pass_file"
-
-# GIT_ASKPASS="$pass_file" 
 git clone https://github.com/eagledb14/dotfiles.git
-rm $pass_file
 
 cd ./dotfiles
 ln -r -s -f ./* ~/.config/
