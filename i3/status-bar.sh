dc7d49ba2966c9f5bc80d68026f2b9e22db0bd8d
eagledb14 <eagledb14@gmail.com>
2023-06-03 23:23:06 -0600

    made the default status bar for desktop

--- i3/status-bar.sh
+++ i3/status-bar.sh
@@ -6,16 +6,17 @@ do
   date=$(date +"%a %b %e, %H:%M:%S")
   volume=$(pactl list sinks | grep -A 11 -e 'State: RUNNING' -e 'State: IDLE' | grep -m 1 Volume | awk '{print $5}' ORS='')
 
-  battery="$(cat /sys/class/power_supply/BAT0/capacity)%"
-  bat_status=$(cat /sys/class/power_supply/BAT0/status)
+  # battery="$(cat /sys/class/power_supply/BAT0/capacity)%"
+  # bat_status=$(cat /sys/class/power_supply/BAT0/status)
 
-  if [ "$bat_status" != "Discharging" ]; then
-    battery="$bat_status"
-  fi
+  # if [ "$bat_status" != "Discharging" ]; then
+  #   battery="$bat_status"
+  # fi
 
-  brightness=$(brightnessctl | grep "Current" | awk '{gsub(/[()]/, ""); print $4}')
+  # brightness=$(brightnessctl | grep "Current" | awk '{gsub(/[()]/, ""); print $4}')
 
 
-  echo "$date | Brightness: $brightness | Battery: $battery |"
+  # echo "$date | Brightness: $brightness | Battery: $battery |"
+  echo "$date | Volume: $volume |"
   sleep 0.1
 done
